622b1abd33a6a1fa7f07e818233e03e89f778393
Jagdev Mishra <Jagdev.Mishra@wolterskluwer.com>
2023-06-14 16:28:13 +0530

    init

---
+++ TodoApp/TodoApp.Frontend/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using TodoApp.Frontend.Models;
+using Dapr.Client;
+
+namespace TodoApp.Frontend.Controllers
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Done { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        const string storeName = "statestore";
+        const string key = "counter";
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            var result = await client.GetAsync("https://todobackend.internal.jollystone-85c1e273.eastus.azurecontainerapps.io/todos");
+            var text = await result.Content.ReadAsStringAsync();
+            ViewBag.Todos =  JsonConvert.DeserializeObject<List<Todo>>(text);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
